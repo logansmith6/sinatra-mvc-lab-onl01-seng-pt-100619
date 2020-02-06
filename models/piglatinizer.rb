f7fb37cd0eb5077366e352f0a86408fe52c54b08
Logan Smith <logsmith06@gmail.com>
2020-02-06 22:35:48 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -39,12 +39,12 @@ def piglatinize(word)
           count += 1
         end
       end
-      if not qu 
+      if not qu
         translate = words[i][count..words[i].length] + translate + 'ay'
         result.push(translate)
-      end 
-    end 
-  end 
+      end
+    end
+  end
       result.join(' ')
 
 end
