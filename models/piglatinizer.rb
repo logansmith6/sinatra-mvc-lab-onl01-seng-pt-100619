d324fb1ff891e389acf2c45248b02c73317d99a8
Logan Smith <logsmith06@gmail.com>
2020-02-06 22:49:22 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,53 +1,21 @@
 class PigLatinizer
-  attr_reader :word
+  attr_reader :str
 
 
 
 
-def piglatinize(word)
-  @word = word
-  word = word_up.downcase
-  vowels = ['a', 'e', 'i', 'o', 'u']
-  words = word.split(' ')
-  result = []
+def piglatinize(str)
+  alpha = ('a'..'z').to_a
+  vowels = %w[a e i o u]
+  consonants = alpha - vowels
 
-  words.each_with_index do |word, i|
-    translate = ''
-    qu = false
-    if vowels.include? word[0]
-      translate = word + 'ay'
-      result.push(translate)
-    else
-      word = word.split('')
-      counter = 0
-      word.each_with_index do |char, index|
-        if vowels.include? char
-          if char == 'u' && translate[-1] == 'q'
-            qu = true
-            translate = words[i][count + 1..words[i].length] + translate + 'uay'
-            result.push(translate)
-            next
-          end
-          break
-        else
-          if char == 'q' && word[i+1] == 'u'
-            qu = true
-            translate = words[i][count+2..words[i].length] + 'quay'
-            result.push(translate)
-            next
-          else
-            translate += char
-          end
-          count += 1
-        end
-      end
-      if not qu
-        translate = words[i][count..words[i].length] + translate + 'ay'
-        result.push(translate)
-      end
-    end
+  if vowels.include?(str[0])
+    str + 'ay'
+  elsif consonants.include?(str[0]) && consonants.include?(str[1])
+    str[2..-1] + str[0..1] + 'ay'
+  elsif consonants.include?(str[0])
+    str[1..-1] + str[0] + 'ay'
+  else
+    str # return unchanged
   end
-      @word = result.join(' ')
-
-   end
 end
