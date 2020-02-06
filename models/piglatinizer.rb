fb18c29c028e76b5bb23faa5fbe2d93603093aef
Logan Smith <logsmith06@gmail.com>
2020-02-06 22:33:57 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,6 +3,43 @@ class PigLatinizer
 
 
 
+def piglatinize(word)
+  @word = word.downcase
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  words = word.split(' ')
+  ordway = []
+
+  words.each_with_index do |word, i| 
+    translate = ''
+    qu = false 
+    if vowels.include? word[0]
+      translate = word + 'ay'
+      result.push(translate)
+    else 
+      word = word.split('')
+      counter = 0 
+      word.each_with_index do |char, index|
+        if vowels.include? char 
+          if char == 'u' and translate[-1] == 'q'
+            qu = true 
+            translate = words[i][count + 1..words[i].length] + translate + 'uay'
+            result.push(translate)
+            next 
+          end 
+          break
+        else 
+          if char == 'q' and word[i+1] == 'u' 
+            qu = true 
+            translate = words[i][count+2..words[i].length] + 'quay'
+            result.push(translate)
+            next 
+          else 
+            translate += char 
+          end 
+          count += 1
+        end 
+      end 
+end 
 
 
 
